7d7b9fc8f39e1812467e9620d1af26e9293d4103
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-11-06 14:23:47 +0600

    netlify

---
+++ build/precache-manifest.c452a0447dd509ff2c94dab76ec8470b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c77e3fe96e3aed770ccb",
+    "url": "./static/css/main.8a044445.chunk.css"
+  },
+  {
+    "revision": "c77e3fe96e3aed770ccb",
+    "url": "./static/js/main.c77e3fe9.chunk.js"
+  },
+  {
+    "revision": "62e9fc26fbc0ae23143b",
+    "url": "./static/css/1.8a5f9553.chunk.css"
+  },
+  {
+    "revision": "62e9fc26fbc0ae23143b",
+    "url": "./static/js/1.62e9fc26.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "45a5969bb9412fa42063d36743a2aab9",
+    "url": "./index.html"
+  }
+];
