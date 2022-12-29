ed8367c1f4867f4f4e9b021e89cc50fa736caba0
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-12-29 19:23:41 +0600

    fucking blogs

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dbd289f68807821c02c0d2085cfe0eaa.js"
+  "./precache-manifest.06dc44eeea8ad696e27142ddd6fcf3b1.js"
 );
 
 workbox.clientsClaim();
