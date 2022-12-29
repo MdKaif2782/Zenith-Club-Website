09eb043747bc39549c95b6802a1f1829cb479b97
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-12-29 20:08:13 +0600

    redirects

---
+++ build/precache-manifest.b9a28f4fdaace430cfd85e3962d45a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66b564a135a055cc94be",
+    "url": "./static/css/main.fc14440b.chunk.css"
+  },
+  {
+    "revision": "66b564a135a055cc94be",
+    "url": "./static/js/main.66b564a1.chunk.js"
+  },
+  {
+    "revision": "67cadb9aaf475d347e9b",
+    "url": "./static/css/1.fd98734e.chunk.css"
+  },
+  {
+    "revision": "67cadb9aaf475d347e9b",
+    "url": "./static/js/1.67cadb9a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f739b974a99ef1d0686e063b7215ac30",
+    "url": "./index.html"
+  }
+];
